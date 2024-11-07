907665a7041a00903749a4cc9288bef1c955cdc6
Val Packett <val@packett.cool>
2024-11-06 22:04:46 -0300

    Do not try to use _udiv128 on clang-cl
    That intrinsic is currently not supported.
    See https://github.com/llvm/llvm-project/issues/59168

--- r128.h
+++ r128.h
@@ -815,7 +815,7 @@ static const r128__udiv128Proc r128__udiv128 = (r128__udiv128Proc)(void*)r128__u
 #else
 static R128_U64 r128__udiv128(R128_U64 nlo, R128_U64 nhi, R128_U64 d, R128_U64 *rem)
 {
-#if defined(_M_X64) && !defined(R128_STDC_ONLY) && !defined(__MINGW32__)
+#if defined(_M_X64) && !defined(R128_STDC_ONLY) && !defined(__MINGW32__) && !defined(__clang__)
    return _udiv128(nhi, nlo, d, rem);
 #elif defined(__x86_64__) && !defined(R128_STDC_ONLY)
    R128_U64 q, r;
